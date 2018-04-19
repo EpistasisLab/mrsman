9e1efbc768ab5e8aef69c21acd1cf8a1b6731aae
Steven Vitale <svitale@upenn.edu>
2018-04-19 16:25:12 -0400

    getting rid of db

--- js/import.js
+++ js/import.js
@@ -3,249 +3,221 @@
 // http://arxiv.org/pdf/1608.00570.pdf 
 
 // basic requires
-var http = require('http');
-var mysql = require('mysql');
-var Q = require("q"); // v2.11.1
+const http = require('http');
+const Q = require("q"); // v2.11.1
 // record creation
-var luhn = require('luhn-mod-n');
-var randomname = require('node-random-name')
-var rp = require('request-promise');
+const luhn = require('luhn-mod-n');
+const randomname = require('node-random-name')
+const rp = require('request-promise');
+const request = require('request');
 //ui
-var blessed = require('blessed');
-var contrib = require('blessed-contrib');
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+const fs = require('fs')
+const transform = require('stream-transform');
+const csv = require("fast-csv");
+
+
 // configs
 var config = require('./config');
 
-// Define our connection to the MySQL database.
-var pool = mysql.createPool({
-    host: config.mysql_host,
-    user: config.mysql_user,
-    password: config.mysql_pass,
-    database: config.mysql_dbname,
-    connectionLimit: 20, // Default value is 10.
-    waitForConnections: true, // Default value.
-    queueLimit: 0 // Unlimited - default value.
-});
-
-var db = {
-    query: function(sql, params) {
-        var deferred = Q.defer();
-        pool.query(sql, params, deferred.makeNodeResolver());
-        return (deferred.promise);
-    }
-};
+// globals
+//number of cuncurrent http requests
+const poolsize = 10;
+//
+//number of records proccessed
+var count = 0;
+//number of failed requests
+var failcount = 0;
 
 //ui 
 var screen = blessed.screen()
-
-var grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
-
-//var gauge = grid.set(1, 1, 2, 4, contrib.gauge, {label: 'Progress', stroke: 'green', fill: 'white'})
- var gauge = grid.set(0, 0, 4, 12, contrib.gauge,
-    { label: 'Progress'
-    , stroke: 'green'
-    , fill: 'white'});
+var grid = new contrib.grid({
+    rows: 12,
+    cols: 12,
+    screen: screen
+})
+var gauge = grid.set(0, 0, 4, 12, contrib.gauge, {
+    label: 'Progress',
+    stroke: 'green',
+    fill: 'white'
+});
 screen.append(gauge);
-
 gauge.setPercent(0)
-
-var log = grid.set(4, 1, 4, 10, contrib.log,
-  { fg: "green"
-  , selectedFg: "green"
-  , label: 'Status'});
-
+gauge.setStack([{
+    percent: 0,
+    stroke: 'green'
+}, {
+    percent: 0,
+    stroke: 'magenta'
+}, {
+    percent: 100,
+    stroke: 'cyan'
+}])
+var log = grid.set(4, 1, 4, 10, contrib.log, {
+    fg: "green",
+    selectedFg: "green",
+    label: 'Status'
+});
 screen.append(log);
-
-
-
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
 });
-
 screen.on('resize', function() {
-  gauge.emit('attach');
-  log.emit('attach');
+    gauge.emit('attach');
+    log.emit('attach');
 });
-
 screen.render()
 
-// global
-//number of records to process/thread
-var batchsize = 100;
-
-//number of records to process
-var total = 0;
-
-//number of records proccessed
-var count = 0;
-
-//format source patient record and import
-var formatPatient = function(record) {
-    var gender = record.PatientGender;
-    var id = String(record.id);
-    var OpenMRSID = id + luhn.generateCheckCharacter(id, '0123456789ACDEFGHJKLMNPRTUVWXY');
+var fhirPatient = function(record) {
+    var gender = record.PatientGender.toLowerCase();
     var identifier = record.PatientID.toLowerCase()
-    var birthdate = record.PatientDateOfBirth.toISOString();
+    var birthdate = new Date(record.PatientDateOfBirth).toISOString().substr(0, 10);
+    var id = record.PatientID.substring(0, 8).replace(/B/g, 'H');
+    var OpenMRSID = id + luhn.generateCheckCharacter(id, '0123456789ACDEFGHJKLMNPRTUVWXY');
     var givenName = randomname({
         random: Math.random,
         first: true,
-        gender: gender.toLowerCase()
+        gender: gender
     })
     var familyName = randomname({
         random: Math.random,
         last: true
     })
-    var person = {
-        "names": [{
-            "givenName": givenName,
-            "familyName": familyName
+
+    return {
+        "resourceType": "Patient",
+        "identifier": [{
+            "use": "usual",
+            "system": "OpenMRS ID",
+            "value": OpenMRSID
         }],
-        //format gender between source and destination db
-        "gender": gender.substring(0, 1),
-        "birthdate": birthdate,
+        "name": [{
+            "use": "usual",
+            "family": familyName,
+            "given": [
+                givenName
+            ]
+        }],
+        "gender": gender,
+        "birthDate": birthdate,
+        "deceasedBoolean": false,
+        "active": true
     }
-    var identifiers = [{
-        "identifierType": "8d79403a-c2cc-11de-8d13-0010c6dffd0f", //identifies this as an "Old OpenMRS ID" type 
-        "identifier": identifier,
-        "location": "8d6c993e-c2cc-11de-8d13-0010c6dffd0f" // Default Location
-    },{
-   "identifierType": "05a29f94-c0ed-11e2-94be-8c13b969e334", // OpenMRS ID
-    "identifier": OpenMRSID, // valid Luhn mod 30 check digit
-    "location": "8d6c993e-c2cc-11de-8d13-0010c6dffd0f" // Default Location
-    }];
-    var patient = {
-        "person": person,
-        "identifiers":identifiers,
-    }
-    return patient;
-
 
 }
 
 
-//get the number of records to be imported
-// group  = null or a hex value from 0-f
-var getNum = function(group) {
-    var sql = 'SELECT count(*) num from Patients where PatientID not in (select PatientID from OpenMRSPatients)'
-    if (group) {
-        sql+=' and RIGHT(MD5(PatientId),1) = "'+group+'"';
+var importPatient = function(patient) {
+    var options = {
+        method: 'POST',
+        uri: config.url,
+        headers: {
+            "Authorization": config.auth
+        },
+        json: patient // Automatically stringifies the body to JSON
+    };
+    var promise = rp(options)
+    return (promise);
+}
+
+var to = 100;
+var from = 0;
+var output = [];
+//var parser = parse({delimiter: '\t',to: to,from: from})
+//var filename = '../data/PatientCorePopulatedTable_short.txt';
+var filename = '../data/PatientCorePopulatedTable.txt';
+//input.pipe(parser).pipe(transformer).pipe(process.stdout);
+//var input = '#Welcome\n"1","2","3","4"\n"a","b","c","d"';
+//console.log(output);
+//parse(input, {delimiter: '\t',to: to,from: from}, function(err, output){
+//});
+
+/*
+var formatStream = csv
+        .createWriteStream({headers: true})
+        .transform(function(obj){
+            var patient=formatPatient(obj);
+            return patient;
+        });
+csv
+   .fromPath(filename, {headers: true, delimiter: '\t'})
+   .pipe(formatStream)
+*/
+function httpRequest(count) {
+    if (count == undefined) {
+        count = 0
     }
-    db.query(sql).then(
-        function handleSettled(snapshots) {
-            total = snapshots[0][0].num;
-            log.log(total + ' records to import');
-            if(total == 0) {
-               log.log('no records to import');
-               pool.end()
-            } else {
-               log.log('start import');
-               importBatch(group);
+    count++
+    var percent_success = ((count - failcount) / output.length).toFixed(2);
+    var percent_fail = (failcount / output.length).toFixed(2);
+    var percent_left = ((output.length - count) / output.length).toFixed(2);
+    gauge.setStack(
+        [{
+                percent: percent_success,
+                stroke: 'green'
+            },
+            {
+                percent: percent_fail,
+                stroke: 'magenta'
+            },
+            {
+                percent: percent_left,
+                stroke: 'cyan'
             }
-        }
-    );
-
-}
-var importBatch = function(group) {
-    var pp = Q.defer();
-    var sql = 'SELECT * from Patients where PatientID not in (select PatientID from OpenMRSPatients)';
-    if(group) {
-        sql+=' and RIGHT(MD5(PatientId),1) = "'+group+'"';
+        ]);
+    if (count > output.length) {
+        return
     }
-    sql+=' limit ' + batchsize + ';';
-    db.query(sql).then(
-        function handleSettled(snapshots) {
-            if (snapshots[0].length > 0) {
-                var promises = snapshots[0].map(
-                    function iterator(row) {
-                        var patient = formatPatient(row);
-                        var options = {
-                            method: 'POST',
-                            uri: config.url,
-                            headers: {
-                                "Authorization": config.auth
-                            },
-                            json: patient // Automatically stringifies the body to JSON
-                        };
-
-                        var promise = rp(options)
-                            .then(function(parsedBody) {
-                                var retArray = Object.assign(parsedBody, patient, row)
-                                return (retArray);
-                            })
-                            .catch(function(err) {
-                                return (false);
-                                console.log(err);
-                                // POST failed...
-                            })
-                        return (promise);
-                    }
-                ); // END: Map loop.
-            } else {
-                log.log('done');
-                pool.end()
-                return;
-            }
-
-            Q.allSettled(promises)
-                .then(
-                    function handleSettled(sn) {
 
 
 
 
-                        var promises2 = sn.map(
-                            function iterator(r) {
 
-                                if (r.value) {
-                                    var record = r.value;
-                                    var UUID = record.uuid;
-                                    var PatientID = record.PatientID;
-                                    var FirstName = record.person.names[0].givenName;
-                                    var LastName = record.person.names[0].familyName;
-                                    var sql = 'insert into OpenMRSPatients (`UUID`,`PatientID`,`FirstName`,`LastName`) values ("' + UUID + '","' + PatientID + '","' + FirstName + '","' + LastName + '");';
-                                    var promise = db
-                                        .query(sql)
-                                        .then(
-                                            function handleResponse(results) {
-                                                return 1;
-
-                                            },
-                                            function handleError(error) {
-                                                return 0
-                                            }
-                                        );
-                                    return promise;
-                                } else {
-                                    return (Q.reject(r.value));
-                                }
-                            })
-
-                        Q.allSettled(promises2)
-                            .then(
-                                function handleSettled(sn2) {
-
-                                    cc = 0;
-                                    for (var i = 0; i < sn2.length; i++) {
-                                        if (sn2[i].state == 'fulfilled') {
-                                            cc = cc + sn2[i].value;
-                                        }
-                                    }
-
-                                    count = count + cc;
-                                    var percent = (count / total);
-                                    log.log(count + " of " + total);
-                                    //screen.render()
-                                    gauge.setPercent(percent)
-                                    importBatch(group)
-
-                                });
+    var patient = output[count];
+    var p = importPatient(patient);
+    p.then(function(parsedBody) {
+            log.log('success')
+            sequentialRequest(count);
+            // POST succeeded...
+        })
+        .catch(function(err) {
+            failcount++
+            log.log('fail')
+            httpRequest(count);
+        });
 
 
 
-                    }
-                );
-        }
-    );
-    return (pp);
 }
-getNum('2');
+
+
+var stream = fs.createReadStream(filename);
+csv
+    .fromStream(stream, {
+        headers: true,
+        delimiter: '\t'
+    })
+    .transform(function(obj) {
+        var patient = fhirPatient(obj);
+        return patient;
+    })
+    .validate(function(data) {
+        return data.resourceType !== undefined; //all persons must be under the age of 50
+    })
+    .on("data-invalid", function(data) {
+        //do something with invalid row
+    })
+    .on("data", function(data) {
+        //importPatient(data);
+        output.push(data);
+    })
+    .on("end", function() {
+        console.log("done");
+        total = output.length;
+        console.log("Running sequential requests!")
+        httpRequest()
+
+
+
+    });
