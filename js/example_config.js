db2a12f4acf5776b5a2e865ce8a869e554639698
Steven Vitale <svitale@upenn.edu>
2018-05-01 11:01:58 -0400

    cleanup

--- js/example_config.js
+++ js/example_config.js
@@ -1,6 +1,14 @@
-exports.mysql_user = "mrsuser";
-exports.mysql_pass = "mrspass";
+exports.mysql_user = "root";
+exports.mysql_pass = "<password>";
 exports.mysql_host= "127.0.0.1";
-exports.mysql_dbname= "demo";
-exports.url='http://localhost:8080/openmrs/ws/rest/v1/patient';
+exports.mysql_dbname= "openmrs";
+exports.url='http://localhost:8080/openmrs/ws';
 exports.auth = "Basic " + new Buffer('admin:Admin123').toString("base64");
+exports.files = {
+                  'Patient':'../data/PatientCorePopulatedTable_short.txt',
+                  'Encounter':'../data/AdmissionsCorePopulatedTable_short.txt',
+                  'Observation':'../data/LabsCorePopulatedTable_short.txt',
+                  'Concept':'../tmp/Concepts.txt',
+                }
+exports.neo4j_user = "neo4j";
+exports.neo4j_pass = "<password>";
