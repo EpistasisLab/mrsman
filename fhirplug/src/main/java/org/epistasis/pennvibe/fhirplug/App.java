2f241a74f68352eaec7737c6a1e9f7f1e21e9489
Steven Vitale <svitale@upenn.edu>
2018-05-07 09:30:14 -0400

    get ready for cleanup

--- fhirplug/src/main/java/org/epistasis/pennvibe/fhirplug/App.java
+++ fhirplug/src/main/java/org/epistasis/pennvibe/fhirplug/App.java
@@ -1,17 +1,23 @@
 package org.epistasis.pennvibe.fhirplug;
-import org.hl7.fhir.dstu3.model.Bundle;
+import java.util.Date;
+
 import org.hl7.fhir.dstu3.model.Bundle.BundleType;
 import org.hl7.fhir.dstu3.model.Bundle.HTTPVerb;
 import org.hl7.fhir.dstu3.model.Enumerations;
 import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.Observation.ObservationStatus;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Period;
 import org.hl7.fhir.dstu3.model.Quantity;
 import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IIdType;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.okhttp.client.OkHttpRestfulClientFactory;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IClientInterceptor;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
@@ -27,65 +33,30 @@ public class App
     {
         System.out.println( "Hello World!" );
     
-     // Create a patient object
-        Patient patient = new Patient();
-        patient.addIdentifier()
-           .setSystem("http://localhost:8082/openmrs/ws/fhir")
-           .setValue("12345");
-        patient.addName()
-           .setFamily("Jameson")
-           .addGiven("J")
-           .addGiven("Jonah");
-        patient.setGender(Enumerations.AdministrativeGender.MALE);
-         
-        // Give the patient a temporary UUID so that other resources in
-        // the transaction can refer to it
-        patient.setId(IdDt.newRandomUuid());
-         
-        // Create an observation object
         Observation observation = new Observation();
-        observation.setStatus(ObservationStatus.FINAL);
         observation
            .getCode()
               .addCoding()
-                 .setSystem("http://loinc.org")
-                 .setCode("789-8")
-                 .setDisplay("Erythrocytes [#/volume] in Blood by Automated count");
+                 .setSystem("http://ciel.org")
+                 .setCode("5087")
+                 .setDisplay("Pulse");
         observation.setValue(
            new Quantity()
-              .setValue(4.12)
-              .setUnit("10 trillion/L")
+              .setValue(60)
+              .setUnit("rate/min")
               .setSystem("http://unitsofmeasure.org")
-              .setCode("10*12/L"));
+              .setCode("rate/min"));
+        
+        Period period = new Period();
+    	period.setStart(new Date());
+    	period.setEnd(new Date());
+    	observation.setEffective(period);
+       // observation.setEffective(value)
+      //  observation.set
          
-        // The observation refers to the patient using the ID, which is already
-        // set to a temporary UUID 
-        observation.setSubject(new Reference(patient.getId()));
-         
-        // Create a bundle that will be used as a transaction
-        Bundle bundle = new Bundle();
-        bundle.setType(BundleType.TRANSACTION);
-         
-        // Add the patient as an entry. This entry is a POST with an
-        // If-None-Exist header (conditional create) meaning that it
-        // will only be created if there isn't already a Patient with
-        // the identifier 12345
-        bundle.addEntry()
-           .setFullUrl(patient.getId())
-           .setResource(patient)
-           .getRequest()
-              .setUrl("Patient")
-              .setIfNoneExist("identifier=http://localhost:8082/openmrs/ws/fhir|12345")
-              .setMethod(HTTPVerb.POST);
-         
-        // Add the observation. This entry is a POST with no header
-        // (normal create) meaning that it will be created even if
-        // a similar resource already exists.
-        bundle.addEntry()
-           .setResource(observation)
-           .getRequest()
-              .setUrl("Observation")
-              .setMethod(HTTPVerb.POST);
+
+        
+
          
         // Log the request
         FhirContext ctx = FhirContext.forDstu3();
@@ -101,22 +72,26 @@ public class App
   // Use OkHttp
      ctx.setRestfulClientFactory(new OkHttpRestfulClientFactory(ctx));
       
-     // Create the client
-//     IGenericClient client = ctx.newRestfulGenericClient("http://localhost:9999/fhir");
-        
-        
- //       System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(bundle));
-         
-        // Create a client and post the transaction to the server
         IGenericClient client = ctx.newRestfulGenericClient("http://localhost:8082/openmrs/ws/fhir");
         client.registerInterceptor(authInterceptor);
-           
+       
+        // search for patient 123
+    //    Patient patient = client.read()
+    //                            .resource(Patient.class)
+    //                            .withId("acf17485-eb07-4e80-9e26-43099bd41ab5")
+    //                            .execute();
+       
+      //  observation.set
+   //     observation.setSubject(new Reference("Patient/eb552673-a137-4e52-b488-cd662de3e9f5"));
+        Reference reference = new Reference();
+        reference.setReference("Patient/eb552673-a137-4e52-b488-cd662de3e9f5");
+        //observation.setSubject(new Reference(patient.getId()));
+        observation.setSubject(reference);
+        System.out.println(observation);
         
-        Bundle resp = client.transaction().withBundle(bundle).execute();
-         
-        // Log the response
-        System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(resp));
-    
+        MethodOutcome resp = client.create().resource(observation).encodedJson().execute();
+        System.out.println(resp);
+
     
     
     }
