6b627ece60b0e2207c3af0f9075cc76854002286
Steven Vitale <svitale@upenn.edu>
2018-06-04 16:14:01 -0400

    working on formatting

--- python/progression.py
+++ python/progression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from neo4jrestclient import client
 from neo4jrestclient.client import GraphDatabase
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="password")
+db = GraphDatabase("http://127.0.0.2:7474", username="neo4j", password="password")
 import pandas as pd
 import numpy as np
 import json
@@ -16,7 +16,10 @@ from matplotlib import pyplot
  
 print("query observations")
 #q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})-[spawned]-(e:Encounter)-[encountered]-(p:Patient)-[encounter]-(e2:Encounter)-[]-(o2:Observation) Return p.id as patient_id,e2.id as encounter_id,o2.display as observation,o2.value as value,o2.timestamp as timestamp order by p.id,o2.timestamp"
-q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE', value:'WHO STAGE 3 ADULT'})-[spawned]-(e:Encounter)-[encountered]-(p:Patient)-[]-(e2:Encounter)-[]-(ob:Observation  {display:'CURRENT WHO HIV STAGE'}) where not ob.value =  o.value  with p match (p)--(e3:Encounter)--(o3:Observation) Return distinct p.id as patient_id,e3.id as encounter_id,o3.display as observation,o3.value as value,o3.timestamp as timestamp order by p.id,o3.timestamp"
+#q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})-[spawned]-(e:Encounter)-[encountered]-(p:Patient)-[]-(e2:Encounter)-[]-(ob:Observation  {display:'CURRENT WHO HIV STAGE'}) where not ob.value =  o.value  with p match (p)--(e3:Encounter)--(o3:Observation) Return distinct p.id as patient_id,e3.id as encounter_id,o3.display as observation,o3.value as value,o3.timestamp as timestamp order by p.id,o3.timestamp"
+#q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})--(e:Encounter) with e match (p:Patient)--(e)--(o:Observation)  Return p.id as patient_id,e.id as encounter_id,o.display as observation,o.value as value,o.timestamp as timestamp order by p.id,o.timestamp"
+#q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})--(e:Encounter) with e match (p:Patient)--(e)--(o:Observation)--(c:Concept) where c.class = 'Test' or c.class = 'Finding' Return p.id as patient_id,e.id as encounter_id,o.display as observation,c.class as class,c.type as type,o.value as value,o.timestamp as timestamp order by p.id,o.timestamp"
+q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})--(e:Encounter) with e match (p:Patient)--(e)--(o:Observation)--(c:Concept) where c.class = 'Test' or o.display = 'CURRENT WHO HIV STAGE' Return p.id as patient_id,e.id as encounter_id,o.display as observation,o.value as value,o.timestamp as timestamp order by p.id,o.timestamp"
 
 results = db.query(q, data_contents=True)
 columns = results.columns;
@@ -105,6 +108,8 @@ pid_col = max_observation + 1
 ts_col = max_observation + 2
 #staging column
 stage_col = max_observation + 3
+last_col = stage_col
+last_col = 4 
 #
 encounters = np.zeros((max_encounter+1,max_observation+4))
 for i,row in observations.iterrows():
@@ -114,18 +119,19 @@ for i,row in observations.iterrows():
   encounters[row.encounter_id,stage_col] = row.stage
 
 
+encounters = encounters[1:max_encounter,1:max_observation+4]
+np.savetxt("data/csv/encounters.csv", encounters, delimiter=",")
 #remove first row
-encounters = encounters[1:]
 #remove stage col
 endpoints = encounters[...,stage_col]
-obs = encounters[...,:stage_col]
+obs2 = encounters[...,:last_col]
 
-x_train, x_test, y_train, y_test = train_test_split(obs,endpoints,  test_size=0.1, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(obs2,endpoints,  test_size=0.4, random_state=0)
 
 
 
 model = Sequential()
-model.add(Dense(units=64, activation='relu', input_dim=stage_col))
+model.add(Dense(units=64, activation='relu', input_dim=last_col))
 model.add(Dense(units=5, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer='sgd',
@@ -136,7 +142,7 @@ model.compile(loss='sparse_categorical_crossentropy',
 y_train_binary = to_categorical(y_train)
 y_test_binary = to_categorical(y_test)
 
-history=model.fit(x_train, y_train, epochs=15, batch_size=1, validation_data=(x_test, y_test), verbose=2, shuffle=False)
+history=model.fit(x_train, y_train, epochs=60, batch_size=100, validation_data=(x_test, y_test), verbose=2, shuffle=False)
 #model.fit(x_train, y_train, epochs=144, batch_size=32)
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 classes = model.predict(x_test, batch_size=128)
