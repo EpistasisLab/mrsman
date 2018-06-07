242c9fdcb8ed51cfa7148838c2b0362b2340f1c0
Steven Vitale <svitale@upenn.edu>
2018-06-07 09:49:49 -0400

    calculate importance scores

--- python/progression.py
+++ python/progression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from neo4jrestclient import client
 from neo4jrestclient.client import GraphDatabase
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="password")
+db = GraphDatabase("http://127.0.0.2:7474", username="neo4j", password="password")
 import pandas as pd
 import numpy as np
 import json
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from matplotlib import pyplot
 
- 
 print("query observations")
 #q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})--(e:Encounter) with e match (p:Patient)--(e)--(o:Observation)--(c:Concept) where c.class = 'Test' or o.display = 'CURRENT WHO HIV STAGE' Return p.id as patient_id,e.id as encounter_id,o.display as observation,o.value as value,o.timestamp as timestamp order by p.id,o.timestamp"
 #q="MATCH (o:Observation {display:'CURRENT WHO HIV STAGE'})--(e:Encounter) with e match (p:Patient)--(e)--(o:Observation)--(c:Concept)  Return p.id as patient_id,e.id as encounter_id,o.display as observation,o.value as value,o.timestamp as timestamp order by p.id,o.timestamp"
@@ -26,18 +25,41 @@ observations = pd.DataFrame.from_records(results.rows,columns=columns)
 
 print("create lookup table")
 lookup = {}
-for var in columns:
-    i = 0
+for var in ['patient_id','encounter_id']:
+    i = 1
     vals = observations[var].unique()
     lookup[var] = {}
     for val in vals:
-        try:
-          f=float(val)
-        except ValueError:
-          i += 1
-          lookup[var][val] = i
+      lookup[var][val] = i
+      i += 1
 
-print("processing stages")
+observation_summary = observations.groupby(['observation', 'value']).size().reset_index().rename(columns={0:'count'})
+lookup['observation'] = {}
+lookup['observation_values'] = {}
+
+i=0
+for obs in observation_summary['observation'].unique():
+  lookup['observation'][obs] = i;
+  values = observation_summary.loc[observation_summary['observation'] == obs]
+  ar = {}
+  j = 1
+  is_str = False
+  for k,row in values.iterrows():
+    try:
+      f=float(row.value)
+    except ValueError:
+      is_str = True
+      ar[row.value] = j
+      j += 1
+  if(is_str):
+    lookup['observation_values'][obs] = ar
+  i += 1
+
+
+with open('data/json/lookup.json', 'w') as outfile:
+    json.dump(lookup['observation'], outfile)
+
+print("process stages")
 for r in results:
     record = {}
     for i,val in enumerate(r):
@@ -81,20 +103,20 @@ observations['stage'] = 0;
 for i, row in observations.iterrows():
   stage = getstage(row)
   observations.at[i,'stage'] = stage
-  for column in columns:
-    try: 
-      val = lookup[column][row[column]]
-    except:
-      val = row[column]
-    observations.at[i,column] = val
+  for column in ['patient_id','encounter_id','observation']:
+    observations.at[i,column] = lookup[column][row[column]]
+  try:
+    val = lookup['observation_values'][row['observation']][row['value']]
+  except:
+    val = row['value']
+  observations.at[i,'value'] = val
 
-print("saving observations")
+
+print("save observations")
 observations.to_csv('data/csv/observations.csv',index_label=False)
-observations.to_pickle('data/pickle/observations.pkl')
-with open('data/json/lookup.json', 'w') as outfile:
-    json.dump(lookup, outfile)
+#observations.to_pickle('data/pickle/observations.pkl')
 
-print("analyzing")
+print("analyze")
 max_encounter = observations.encounter_id.max()
 max_observation = observations.observation.max()
 #patient id column
@@ -119,16 +141,35 @@ encounters  = np.delete(encounters,0,0)
 #encounters = encounters[1:max_encounter+1,1:max_observation+3]
 endpoints = encounters[...,stage_col_2]
 #remove primary staging column
-encounters  = np.delete(encounters,stage_col_1,1)
+#encounters  = np.delete(encounters,stage_col_1,1)
+encounters[:, stage_col_1] = [0]
 #remove calculated staging column
-np.savetxt("data/csv/encounters.csv", encounters, delimiter=",")
+encs  = encounters.copy()
 encounters  = np.delete(encounters,-1,1)
-np.savetxt("data/csv/endpoints.csv", endpoints, delimiter=",")
+#remove patient ids
+#encounters[:, pid_col] = [0]
+#encounters  = np.delete(encounters,1,1)
+
+from sklearn.ensemble import ExtraTreesClassifier
+etc_model = ExtraTreesClassifier()
+etc_model.fit(encounters, endpoints)
+i=0
+imps = np.zeros([encounters.shape[1],2])
+for imp in etc_model.feature_importances_:
+  imps[i,0]=i;
+  imps[i,1]=imp;
+  i += 1
+
+important = imps[:,1].argsort()[[1,2,3,4,5]]
+print(important)
+encs = encs[:,[index for index in range(encounters.shape[1]) if index not in important]]
+np.savetxt("data/csv/encounters.csv", encs, delimiter=",")
+
+x_train, x_test, y_train, y_test = train_test_split(encounters,endpoints,  test_size=0.2, random_state=0)
 
 #obs2 = encounters[...,:last_col-1]
-x_train, x_test, y_train, y_test = train_test_split(encounters,endpoints,  test_size=0.2, random_state=0)
 model = Sequential()
-model.add(Dense(units=17, activation='sigmoid', input_dim=encounters.shape[1]))
+model.add(Dense(units=64, activation='sigmoid', input_dim=encounters.shape[1]))
 model.add(Dense(units=np.unique(endpoints).size, activation='sigmoid'))
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer='sgd',
@@ -143,5 +184,4 @@ pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
 pyplot.legend()
 pyplot.show()
-
 print("fin")
