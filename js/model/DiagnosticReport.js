a7ca9c46358bf7a30922e75d152ddb5b2d290816
Steven Vitale <steve@bioinf.us>
2019-03-04 11:19:16 -0500

    working on model for diagnostic report

---
+++ js/model/DiagnosticReport.js
@@ -0,0 +1,53 @@
+var util = require('util');
+var Q = require('q');
+var Model = {};
+Model['Base'] = require('./Base');
+Model['Location'] = require('./Location');
+Model['Practitioner'] = require('./Practitioner');
+Model['Observation'] = require('./Observation');
+var ObservationModel = require('./Observation');
+
+
+function EncounterModel(obj) {
+    Model['Base'].apply(this, arguments);
+    this.resourceType = 'Encounter';
+    this.Observation = Model['Observation'];
+    this.Location = Model['Location'];
+    this.Practitioner = Model['Practitioner'];
+    this.format(obj)
+}
+
+
+
+EncounterModel.prototype.format = function(obj) {
+    var obj = JSON.parse(JSON.stringify(obj));
+    if (obj.raw !== undefined) {
+        obj.start = obj.raw.period[0].start[0]['$'].value;
+        obj.end = obj.raw.period[0].end[0]['$'].value;
+    }
+    for (var key in obj) {
+        if (this[key] !== undefined) {
+            if (typeof(this[key]) !== 'function') {
+                this[key] = obj[key];
+            } else {
+                var objects = [];
+                obj[key].map(function(data) {
+                   objects.push(new Model[key](data));
+                });
+                this[key] = objects;
+            }
+
+
+        }
+    }
+    if (obj.id) {
+        this.id = obj.id;
+    }
+    if (this.resourceType !== undefined && obj[this.resourceType] !== undefined) {
+        this.id = obj[this.resourceType][0]['id']
+        delete obj[this.resourceType]
+    }
+}
+
+util.inherits(EncounterModel, Model['Base']);
+module.exports = EncounterModel;
